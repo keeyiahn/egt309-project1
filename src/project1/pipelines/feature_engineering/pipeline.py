c38deb2ae0760e7f60df235dba83ed2c3ac93cb2
milkyru <gcyanting@gmail.com>
2025-06-02 22:04:10 +0800

    pipeline update

--- src/project1/pipelines/feature_engineering/pipeline.py
+++ src/project1/pipelines/feature_engineering/pipeline.py
@@ -2,13 +2,34 @@ from kedro.pipeline import node, pipeline
 from .nodes import *
 
 def create_model_pipeline(**kwargs):
-    return pipeline(
-        [
+    return pipeline([
             node(
                 func=get_dataset_info,
-                inputs="cleaned_dataset",
+                inputs=["cleaned_dataset"],
                 outputs="dataframe"
             ),
+            node(
+                func=preparing_dataset,
+                inputs=["cleaned_dataset"],
+                outputs="dataframe"
 
-        ]
-    )
+            ),
+            node(
+                func=one_hot_encode,
+                inputs=["cleaned_dataset"],
+                outputs="dataframe"
+
+            ),
+            node(
+                func=splitting_buyers,
+                inputs=["cleaned_dataset"],
+                outputs="dataframe"
+            ),
+            node(
+                func=dropping_columns,
+                inputs=["cleaned_dataset"]
+                outputs="dataframe"
+            )
+
+
+    ])
